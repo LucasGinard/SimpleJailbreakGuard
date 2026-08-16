6a20e8c0c6227ed16452da77f320e968831f0f6c
Lucas Ginard <contactolucasginard@gmail.com>
2026-08-16 03:08:53 -0300

    Initial Commit

---
+++ SDKSimpleJailbreakGuard/SDKSimpleJailbreakGuard.swift
@@ -0,0 +1,9 @@
+//
+//  SDKSimpleJailbreakGuard.swift
+//  SDKSimpleJailbreakGuard
+//
+//  Created by Lucas Ginard on 2026-08-16.
+//
+
+import Foundation
+
---
+++ SDKSimpleJailbreakGuardTests/SDKSimpleJailbreakGuardTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDKSimpleJailbreakGuardTests.swift
+//  SDKSimpleJailbreakGuardTests
+//
+//  Created by Lucas Ginard on 2026-08-16.
+//
+
+import Testing
+@testable import SDKSimpleJailbreakGuard
+
+struct SDKSimpleJailbreakGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
